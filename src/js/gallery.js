f01b2b554afe066f006826c927fd70b28efe2ce3
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2024-11-22 22:35:31 +0100

    testing

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,6 +1,6 @@
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
 const images = [
