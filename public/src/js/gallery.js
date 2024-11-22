58f113aceb1663d157b8bc4c9a6fdba5a9193df7
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2024-11-22 23:29:30 +0100

    fix

--- public/src/js/gallery.js
+++ public/src/js/gallery.js
@@ -8,7 +8,7 @@ const images = [
       preview:
         'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
       original:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+        'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg', 
       description: 'Hokkaido Flower',
     },
     {
